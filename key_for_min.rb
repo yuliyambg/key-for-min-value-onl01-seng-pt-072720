ccf4ec299c73b6c955b08398e94a8549753cf394
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-01 23:39:47 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+values_array = name_hash.collect{|key, value| value}.sort
+  name_hash.each{|key, value| return key if value == values_array[0]}
+  nil
 end
